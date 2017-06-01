18325599fd607e5315c9980a3fc727b46e13e777
indrajit <eendroroy@gmail.com>
2017-06-01 09:51:56 +0600

    demo.py

---
+++ demo.py
@@ -0,0 +1,9 @@
+import logtrail
+logtrail.logger.info("calling 'logtrail.logger.info'")
+logtrail.logger.debug("calling 'logtrail.logger.debug'")
+logtrail.logger.warn("calling 'logtrail.logger.warn'")
+logtrail.logger.error("calling 'logtrail.logger.error'")
+logtrail.logger.i("calling 'logtrail.logger.i'")
+logtrail.logger.d("calling 'logtrail.logger.d'")
+logtrail.logger.w("calling 'logtrail.logger.w'")
+logtrail.logger.e("calling 'logtrail.logger.e'")
